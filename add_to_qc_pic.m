705abc86af806cd1d543d33c54aa271068592f43
evieneylon98 <102476503+evieneylon98@users.noreply.github.com>
2024-02-08 17:29:35 +0100

    Update add_to_qc_pic.m
    print arguments octave compatible

--- add_to_qc_pic.m
+++ add_to_qc_pic.m
@@ -4,9 +4,9 @@ function add_to_qc_pic(subject,figure)
 data_path = getenv('DATA_DIR');
 qc_pic_fname = sprintf('%s/%s/qc_%s.ps',data_path,subject,subject);
 if(~exist(qc_pic_fname,'file'))
-    print('-painters','-bestfit','-r150','-noui',figure,'-dpsc',qc_pic_fname);
+    print(figure,'-painters','-bestfit','-r150','-noui','-dpsc',qc_pic_fname);
 else
-    print('-painters','-bestfit','-r150','-noui',figure,'-dpsc','-append',qc_pic_fname);
+    print(figure,'-painters','-bestfit','-r150','-noui','-dpsc','-append',qc_pic_fname);
 end
     
 end
